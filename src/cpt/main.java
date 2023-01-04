21ae4c4bffb4f4425c836e00e2055c6fe55f0abe
Matthew Yam <matthew.yam23@ycdsbk12.ca>
2023-01-04 00:49:27 -0500

    start pi chart

---
+++ src/cpt/main.java
@@ -0,0 +1,14 @@
+package cpt;
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+public class main {
+
+    public static void main(String[] args) throws Exception{
+
+  
+    }
+
+}
+
+
