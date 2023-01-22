46e6653835cc2f1804e0a075810db416e1cbeaf4
Matthew Yam <matthew.yam23@ycdsbk12.ca>
2023-01-22 18:20:47 -0500

    final fixes and javadocs

--- src/cpt/Charts.java
+++ src/cpt/Charts.java
@@ -1,6 +1,5 @@
 package cpt;
 
-
 import javafx.application.Application;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
@@ -28,10 +27,9 @@ import javafx.scene.layout.GridPane;
 
 public class Charts extends Application{
 
+    //Declare Variables
     Reader dataGrabber;
-
     int count = 0;
-
     static boolean textTrigger = false;
     
     @Override
@@ -54,7 +52,6 @@ public class Charts extends Application{
         MenuItem bar = new MenuItem("Bar Chart");
         MenuItem pie = new MenuItem("Pie Chart");
         MenuItem both = new MenuItem("Both Charts");
-        MenuItem confirmReset = new MenuItem("Confirm Reset");
         charts.getItems().add(bar);
         charts.getItems().add(pie);
         charts.getItems().add(both);
@@ -63,19 +60,17 @@ public class Charts extends Application{
         menuBar.setMaxWidth(65);
         area.add(menuBar, 0, 0);
 
+        //Clear button
         Button clear = new Button("Reset");
         clear.setMaxWidth(65);
         area.add(clear, 0, 1);
 
         
-        //Text
-        Text prompt = new Text();
+        //Select chart text prompt
         Text pick = new Text();
-        prompt.setText("Percentage of Population Covered by Health Insurance");
         pick.setText("  ↑ Select a Chart");
         pick.setFont(Font.font(30));
         area.add(pick, 0, 2);
-        prompt.setVisible(true);
         pick.setVisible(true);
 
         if(textTrigger = true){
@@ -83,39 +78,35 @@ public class Charts extends Application{
         }
         
 
-       //Bar chart
+        //Bar chart
+
+        //Set Lables for Bar Chart
         CategoryAxis xAxis = new CategoryAxis();
         xAxis.setLabel("Country");
         NumberAxis yAxis = new NumberAxis();
         yAxis.setLabel("Percent");
 
+        //Adds Data and Title to Bar Chart
         XYChart.Series series1 = new XYChart.Series();
-
         BarChart<String,Number> barc = new BarChart<String,Number>(xAxis,yAxis);
-
-        barc.getData().addAll(series1);     
-       
+        barc.getData().addAll(series1);
+        barc.setTitle("Percentage of Population Covered by Health Insurance");
         for(int loop = 0; loop  < dataGrabber.returnSendCountry().size(); loop++){
             series1.getData().add(new XYChart.Data(dataGrabber.returnSendCountry().get(loop), dataGrabber.returnSendPercent().get(loop)));
         }
 
-        
+        //Tooltips to show exact percent and country name when hovering over bar
         for (final Series<String, Number> series : barc.getData()) {
             for (final Data<String, Number> data : series.getData()) {
                 Tooltip barGrabber = new Tooltip(data.getXValue() +": " + data.getYValue() + "%");
                 Tooltip.install(data.getNode(), barGrabber);			
             }
         }
-        
-
-        barc.setTitle("Percentage of Population Covered by Health Insurance");
-
 
         //Pie Chart
 
-        ObservableList<PieChart.Data> pieChartData = FXCollections.observableArrayList();
-
-        
+        //Add Data to Pie Chart
+        ObservableList<PieChart.Data> pieChartData = FXCollections.observableArrayList(); 
         for(int pieLoop = 0; pieLoop < dataGrabber.returnSendCountry().size(); pieLoop++){
             pieChartData.add(new PieChart.Data(dataGrabber.returnSendCountry().get(pieLoop), dataGrabber.returnSendPercent().get(pieLoop)));
         }
@@ -126,25 +117,25 @@ public class Charts extends Application{
         piec.setData(pieChartData);
         piec.setTitle("Percentage of Population Covered by Health Insurance");
         piec.setClockwise(true);
+        piec.setLabelLineLength(50);
+        piec.setLabelsVisible(true);
+        piec.setStartAngle(180);
 
+        //Add tooltips to show country and exact percent
         piec.getData().forEach(data -> {
             String percentGrabber = String.format("%.2f%%", data.getPieValue());
             Tooltip showPiePercent = new Tooltip(data.getName() +": " + percentGrabber);
             Tooltip.install(data.getNode(), showPiePercent);
         });
 
-        
-        piec.setLabelLineLength(50);
-        piec.setLabelsVisible(true);
-        piec.setStartAngle(180);
-
-
+        //Add both charts to screen
         area.add(barc, 0, 7);
         barc.setVisible(false);
         area.add(piec, 1, 7);
         piec.setVisible(false);
         piec.setMinSize(700,700);
 
+        //Sets action for when Bar Chart button is pressed
         bar.setOnAction(new EventHandler<ActionEvent>(){
             @Override
             public void handle(ActionEvent showBar) {
@@ -159,6 +150,7 @@ public class Charts extends Application{
             
         });
 
+        //Set action for when Pie Chart button is pressed
         pie.setOnAction(new EventHandler<ActionEvent>(){
             @Override
             public void handle(ActionEvent show) {
@@ -172,6 +164,7 @@ public class Charts extends Application{
             
         });
 
+        //Set action for when Both button is pressed
         both.setOnAction(new EventHandler<ActionEvent>(){
             @Override
             public void handle(ActionEvent show) {
@@ -185,6 +178,7 @@ public class Charts extends Application{
             
         });
 
+        //Set action for when Reset button is pressed
         clear.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent show) {
@@ -196,18 +190,13 @@ public class Charts extends Application{
             }
         });
 
-
-        
-       
-
-       
+        //Loads scene
         VBox bChart = new VBox(area);
         Scene scene = new Scene(bChart, 1920, 1080);
         stage.setTitle("Insurance Coverage");
         stage.setScene(scene);
         stage.setHeight(785);
         stage.setWidth(1200);
-        
         stage.show();  
  
     } 
